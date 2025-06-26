233258f2eb3e537d2212b76d1cf667ae5ea89fb8
Onkar Shinde <167012467+onkarshinde77@users.noreply.github.com>
2025-06-26 13:32:28 +0530

    Update frontend.py

--- Frontend/frontend.py
+++ Frontend/frontend.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "https://fast-api-project-iugu.onrender.com"
+API_URL = "https://fast-api-project-iugu.onrender.com/predict"
 
 st.title("Insurence Premium Category Predictor")
 st.markdown("Enter your detainl below :")
